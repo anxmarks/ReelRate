6725319c09592459e377d4fadd82b0286fba452f
anxmarks <anamarks28@outlook.com>
2025-04-03 09:23:58 -0300

    login button

---
+++ laboratorio-desenvolvimento/src/app/login/page.jsx
@@ -0,0 +1,8 @@
+export default function LoginPage() {
+    return (
+      <div className="flex items-center justify-center min-h-screen">
+        <h1 className="text-2xl font-bold">Welcome to the Login Page</h1>
+      </div>
+    );
+  }
+  
--- laboratorio-desenvolvimento/src/app/page.tsx
+++ laboratorio-desenvolvimento/src/app/page.tsx
@@ -1,11 +1,15 @@
 import Header from "@/components/header";
+import LoginButton from "@/components/LoginButton";
+
 
 export default function Home() {
   return (
    
     <>
       <Header></Header>
-    
+      <div className="flex items-center justify-center min-h-screen">
+      <LoginButton />
+    </div>
     
     </>
   );
