b17228ec9542063405eb37c6d2952b290938b317
Gabryelw <gabryelqwer@gmail.com>
2025-04-04 20:54:02 -0300

    Adição da pagina individal de filmes

---
+++ src/app/(public)/movie/[id]/page.tsx
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+type MoviePageProps = {
+  params: {
+    id: string;
+  };
+};
+
+export default async function MoviePage(props: Promise<MoviePageProps>) {
+  const { params } = await props;
+  const { id } = params;
+
+  const res = await axios.get(
+    `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.NEXT_PUBLIC_TMDB_API_KEY}&language=pt-BR`
+  );
+  const movie = res.data;
+
+  return (
+    <div className="min-h-screen bg-zinc-950 text-white p-6">
+      <div className="max-w-4xl mx-auto flex flex-col md:flex-row gap-6">
+        <img
+          src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+          alt={movie.title}
+          className="w-full md:w-1/3 rounded-xl shadow"
+        />
+        <div className="flex-1">
+          <h1 className="text-3xl font-bold mb-2">{movie.title}</h1>
+          <p className="text-yellow-400 font-semibold mb-4">Nota: {movie.vote_average}</p>
+          <p className="text-gray-300">{movie.overview}</p>
+          <p className="text-sm text-gray-500 mt-4">Data de lançamento: {movie.release_date}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/moviesList.tsx
+++ src/components/moviesList.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useMovies } from "@/app/lib/hooks/useMovies";
+import Link from "next/link";
 
 export default function MoviesList() {
   const { data: movies, isLoading, error } = useMovies();
@@ -11,15 +12,17 @@ export default function MoviesList() {
   return (
     <div className="p-6 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-6 bg-zinc-950 text-white min-h-screen">
       {movies.map((movie: any) => (
-        <div key={movie.id} className="bg-zinc-900 p-3 rounded-xl shadow hover:scale-105 transition">
-          <img
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-            alt={movie.title}
-            className="w-full h-72 object-cover rounded"
-          />
-          <h3 className="mt-2 font-semibold text-sm">{movie.title}</h3>
-          <p className="text-yellow-400 text-xs">Nota: {movie.vote_average}</p>
-        </div>
+        <Link href={`/movie/${movie.id}`} key={movie.id}>
+          <div className="bg-zinc-900 p-3 rounded-xl shadow hover:scale-105 transition cursor-pointer">
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+              className="w-full h-72 object-cover rounded"
+            />
+            <h3 className="mt-2 font-semibold text-sm">{movie.title}</h3>
+            <p className="text-yellow-400 text-xs">Nota: {movie.vote_average}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
