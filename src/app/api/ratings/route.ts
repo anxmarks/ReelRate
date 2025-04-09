db8c7008f2255667c556e3ef44a8dcce6a10ec80
Gabryel-w <gabryelqwer@gmail.com>
2025-04-09 14:00:16 -0300

    Ajustes de build na vercel

--- src/app/api/ratings/route.ts
+++ src/app/api/ratings/route.ts
@@ -1,25 +1,29 @@
 import { NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
+import { prisma } from "@/app/lib/prisma"
 
 export async function GET(req: Request) {
-  const { searchParams } = new URL(req.url);
-  const movieTmdbId = parseInt(searchParams.get("movieTmdbId") || "");
+  try {
+    const { searchParams } = new URL(req.url);
+    const movieTmdbIdParam = searchParams.get("movieTmdbId");
+    const movieTmdbId = movieTmdbIdParam ? parseInt(movieTmdbIdParam, 10) : null;
 
-  if (!movieTmdbId) {
-    return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+    if (!movieTmdbId || isNaN(movieTmdbId)) {
+      return NextResponse.json({ error: "ID inválido" }, { status: 400 });
+    }
+
+    const reviews = await prisma.review.findMany({
+      where: { movieTmdbId },
+      select: { rating: true },
+    });
+
+    if (reviews.length === 0) return NextResponse.json({ average: null });
+
+    const total = reviews.reduce((acc, r) => acc + r.rating, 0);
+    const average = total / reviews.length;
+
+    return NextResponse.json({ average: Number(average.toFixed(1)) });
+  } catch (error) {
+    console.error("Erro em /api/ratings:", error);
+    return NextResponse.json({ error: "Erro interno" }, { status: 500 });
   }
-
-  const reviews = await prisma.review.findMany({
-    where: { movieTmdbId },
-    select: { rating: true },
-  }) as { rating: number }[];
-
-  if (reviews.length === 0) return NextResponse.json({ average: null });
-
-  const total = reviews.reduce((acc: number, r) => acc + r.rating, 0);
-  const average = total / reviews.length;
-
-  return NextResponse.json({ average: Number(average.toFixed(1)) });
 }
