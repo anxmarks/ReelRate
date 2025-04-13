8d4930d0ee56a69c30bf89ca224a19a00fb9a71b
Julia Jung <juliazjung@gmail.com>
2025-04-13 11:52:12 -0300

    Add mensagem erro login
    Adicionado mensagem erro no login

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -4,25 +4,35 @@ import Footer from "@/components/Footer";
 import Header from "@/components/header";
 import { signIn } from "next-auth/react";
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Login() {
+  const [error, setError] = useState<string | null>(null);
+  
   return (
     <>
     <Header />
       <div className="flex min-h-screen items-center justify-center bg-[#2d3250] px-4">
         <div className="w-full max-w-md p-8 space-y-6 bg-[#424769] text-white shadow-xl rounded-2xl border border-[#676f9d]/30">
           <h1 className="text-3xl font-bold text-center text-white">Bem-vindo de volta</h1>
+          {error && (<div className="text-red-400 text-center">{error}</div>)}
           <form
             className="space-y-4"
-            onSubmit={(e) => {
+            onSubmit={async (e) => {
               e.preventDefault();
               const formData = new FormData(e.currentTarget);
-              signIn("credentials", {
+              const result = await signIn("credentials", {
                 email: formData.get("email"),
                 password: formData.get("password"),
-                redirect: true,
+                redirect: false,
                 callbackUrl: "/profile",
               });
+
+              if (result?.error) {
+                setError("Email ou senha incorretos.");
+              } else {
+                window.location.href = result?.url || "/profile";
+              }
             }}
           >
             <input
